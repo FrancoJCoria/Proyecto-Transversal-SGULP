75e16f3f715342c20c63196929e15400c7c9100e
Franco Coria <110592868+FrancoJCoria@users.noreply.github.com>
2025-10-01 18:39:00 -0300

    Creacion del proyecto con sus packages

---
+++ src/main/java/Vista/ProyectoTransversalSG.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Vista;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author FRANCO
+ */
+public class ProyectoTransversalSG {
+
+    public static void main(String[] args) {
+   
+            //Agregar un empleado
+            //String sql= "INSERT INTO empleado(dni,apellido,nombre,acceso,estado) VALUES(?,?,?,?,?)";
+            //Prepara la sentencia SQL en la base de datos / Crea el comando SQL
+            //PreparedStatement ps=conexion.prepareStatement(sql);
+            //Asignamos los datos
+            //ps.setInt(1, 45801072);
+            //ps.setString(2,"Coria");
+            //ps.setString(3, "Franco");
+            //ps.setInt(4, 1);
+            //ps.setBoolean(5, true);
+            //Ejecuta el comando SQL
+            //ps.executeUpdate();
+            
+            //Actualizar un empleado
+            //String sql2="UPDATE empleado SET nombre = 'Franquito' WHERE dni=45801072";
+            //PreparedStatement ps2 = conexion.prepareStatement(sql2);
+            //ps2.executeUpdate();
+            
+            
+            //Eliminar fisicamente un empleado
+            //String sql3="DELETE FROM empleado WHERE dni=45801072";
+            //PreparedStatement ps3 = conexion.prepareStatement(sql3);
+            //ps3.executeUpdate();
+
+         
+    }
+}

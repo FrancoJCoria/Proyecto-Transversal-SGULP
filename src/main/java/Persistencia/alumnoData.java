3d30a3d7478668878a09ccecf1cec39d3bbba177
Franco Coria <110592868+FrancoJCoria@users.noreply.github.com>
2025-10-01 20:51:00 -0300

    Clase Conexion Finalizada

--- src/main/java/Persistencia/alumnoData.java
+++ src/main/java/Persistencia/alumnoData.java
@@ -4,10 +4,93 @@
  */
 package Persistencia;
 
+import Modelo.Alumno;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author FRANCO
  */
 public class alumnoData {
+
+    private Connection con = null;
+
+    public alumnoData(Conexion conexion) {
+        this.con = conexion.buscarConexion();
+    }
+
+    public void guardarAlumno(Alumno a) {
+        String sql = "INSERT INTO alumno(dni,apellido,nombre,fechaNacimiento,estado) VALUES(?,?,?,?,?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, a.getDni());
+            ps.setString(2, a.getApellido());
+            ps.setString(3, a.getNombre());
+            ps.setDate(4, Date.valueOf(a.getFechaNacimiento()));
+            ps.setBoolean(5, a.isEstado());
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al cargar la sentencia SQL " + ex.getMessage());
+        }
+    }
+
+    public void buscarAlumno(int id) {
+        String sql = "SELECT * FROM alumno WHERE dni = ?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                System.out.println(" ID " + rs.getInt("idAlumno"));
+                System.out.print(" Dni " + rs.getInt("dni"));
+                System.out.print(" Apellido " + rs.getString("Apellido"));
+                System.out.print(" Nombre " + rs.getString("Nombre"));
+                System.out.println(" Fecha Nac " + rs.getDate("fechaNacimiento"));
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al cargar la sentencia SQL " + ex.getMessage());
+        }
+
+    }
+
+    public void actualizarAlumno(int id, String columna, Object dato) {
+        if (!columna.equals("dni") && !columna.equals("apellido") && !columna.equals("nombre") && !columna.equals("fechaNacimiento") && !columna.equals("estado")) {
+            throw new IllegalArgumentException("Columna no permitida");
+        }
+
+        String sql = "UPDATE FROM alumno SET " + columna + "=? WHERE idAlumno=?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            if (dato instanceof String) {
+                ps.setString(1, (String) dato);
+                ps.setInt(2, id);
+            } else if (dato instanceof Integer) {
+                ps.setInt(1, (int) dato);
+                ps.setInt(2, id);
+            } else if (dato instanceof LocalDate) {
+                ps.setDate(1, (Date) dato);
+                ps.setInt(2, id);
+            } else if (dato instanceof Boolean) {
+                ps.setBoolean(1, (boolean) dato);
+                ps.setInt(2, id);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(alumnoData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
     
+    public void DarBajaAlumno(){
+    
+    }
 }
--- src/main/java/Persistencia/conexion.java
+++ src/main/java/Persistencia/conexion.java
@@ -4,10 +4,52 @@
  */
 package Persistencia;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author FRANCO
  */
-public class conexion {
+public class Conexion {
+
+    private String url;
+    private String usuario;
+    private String password;
+
+    private static Connection conexion = null;
+
+    public Conexion(String url, String usuario, String password) {
+      this.url=url;
+      this.usuario=usuario;
+      this.password=password;
+    }
     
+    public Connection buscarConexion(){
+      if (conexion == null) {
+
+            //Requiere de un try catch porque puede lanzar una excepcion. si la clase que le indico en este caso el driver no existe en el classpath o no puede cargarse larga la excepcion
+            try {
+                Class.forName("org.mariadb.jdbc.Driver"); //cargar la clase y ejecuta su bloque estatico que ya registra el driver en drivermanager
+
+                //Conexion con la bd - tambien requiere de un try catch si ocurre un error al intentar conectarse a la base de datos ej: url incorrecta, usuario o contraseña invalidos, base de datos apagada
+                Connection conexion = DriverManager.getConnection(url, usuario, password);
+
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Error al cargar Driver " + ex.getMessage());
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, "Error al cargar Driver " + e.getMessage());
+            }
+            
+            
+        }
+      
+      return conexion;
+    }
+
 }
+
+
+

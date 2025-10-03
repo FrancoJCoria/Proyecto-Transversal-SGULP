59f11fcfd545127ca65520d7bba13f6832968545
Franco Coria <110592868+FrancoJCoria@users.noreply.github.com>
2025-10-02 23:23:24 -0300

    Clase materiaData Finalizada

--- src/main/java/Vista/ProyectoTransversalSG.java
+++ src/main/java/Vista/ProyectoTransversalSG.java
@@ -1,14 +1,14 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package Vista;
 
 import Modelo.Alumno;
+import Modelo.Materia;
 import Persistencia.Conexion;
 import Persistencia.alumnoData;
+import Persistencia.materiaData;
 import java.time.LocalDate;
-
 
 /**
  *
@@ -17,14 +17,21 @@ import java.time.LocalDate;
 public class ProyectoTransversalSG {
 
     public static void main(String[] args) {
-   
-         Alumno nuevoAlumno=new Alumno(-1,45801072,"Coria","Franco",LocalDate.of(2004,06,07),true);
-         Conexion miConexion =new Conexion("jdbc:mariadb://localhost/gp7universidad","root","");
-         alumnoData alumnoData = new alumnoData(miConexion);
-         //alumnoData.guardarAlumno(nuevoAlumno);
-         //alumnoData.actualizarAlumno(1, "dni", 45801072);
-          //alumnoData.buscarAlumno(1);
-          //alumnoData.DarBajaAlumno(1);
-          //alumnoData.eliminarAlumno(1);
+
+        Alumno nuevoAlumno = new Alumno(-1, 45801072, "Coria", "Franco", LocalDate.of(2004, 06, 07), true);
+        Conexion miConexion = new Conexion("jdbc:mariadb://localhost/gp7universidad", "root", "");
+        alumnoData alumnoData = new alumnoData(miConexion);
+        //alumnoData.guardarAlumno(nuevoAlumno);
+        //alumnoData.actualizarAlumno(1, "dni", 45801072);
+        // alumnoData.buscarAlumno(1);
+        //alumnoData.DarBajaAlumno(1);
+        //alumnoData.eliminarAlumno(1);
+        Materia nuevaMateria = new Materia(-1, "Matematicas", 2, true);
+        materiaData materiaData = new materiaData(miConexion);
+        materiaData.guardarMateria(nuevaMateria);
+         //materiaData.actualizarMateria(1, "nombre", "Ingles");
+        //materiaData.buscarMateria(1);
+        //materiaData.DarBajaMateria(1);
+        //materiaData.eliminarMateria(1);
     }
 }
